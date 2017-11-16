5cc06c86c42120b1c23595a86d3097fac922d31f
Vedangi Jani <vedangirjani@gmail.com>
2017-11-16 14:42:31 -0500

    replacing filter

--- Directives/TypeAhead/Typeahead.filter.js
+++ Directives/TypeAhead/Typeahead.filter.js
@@ -1,12 +1,7 @@
 angular.module('app').filter('search',function(){
     return function(input,term){
-        var out = [];
-      
-        for(var i=0;i<input.length; i++){
-            if(input[i].indexOf(term) > -1){
-                out.push(input[i]);
-            }
-        }
-        return out;
+        return input.filter(function(el){
+            return el.toLowerCase().indexOf(term.toLowerCase()) > -1;
+        })
     }
 });
